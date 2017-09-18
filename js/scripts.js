e978b2dcdceb81228c11699f951067bf74ca94a9
Alan Falcon and James Blocker <alan+james@>
2017-09-18 10:14:29 -0700

    add backend logic, still not working completely

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  $('form').submit(function(event) {
+    var fav1 = $('#favorite1').val();
+    var fav2 = $('#favorite2').val();
+    var fav3 = $('#favorite3').val();
+    var myArrays = [fav1, fav2, fav3];
+    // myArrays.push("someThing");
+
+
+    myArrays.forEach(function(myArray) {
+      alert(myArray);
+      $('#list').append('<li>' + myArrays[myArray] + '</li>');
+    });
+
+
+
+$('.f1').text(fav1);
+$('.f2').text(fav2);
+$('.f3').text(fav3);
+
+
+    event.preventDefault();
+  });
+});
